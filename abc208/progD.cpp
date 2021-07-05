63091bd649d15322c9017d526f2d1ea6014d3644
kyokio6002 <kyokio0725@gmail.com>
2021-07-05 23:08:57 +0900

    Floyd–Warshall Algorithm

--- abc208/progD.cpp
+++ abc208/progD.cpp
@@ -1,17 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define INF 1000000007
+using ll = long long;
+
 int main(){
 
   int n,m;
   cin >> n >> m;
-  vector<vector<pair<int,int>>> edge(n+1);
+  vector<vector<int>> dp(n,vector<int>(n,INF));
+  for(int i=0;i<n;i++)dp[i][i]=0;
 
   int a,b,c;
   for(int i=0;i<m;i++){
     cin >> a >> b >> c;
-    edge[a].push_back(make_pair(b,c));
+    a--,b--;
+    dp[a][b]=c;
   }
 
+  ll ans=0;
+  for(int k=0;k<n;k++){
+    for(int s=0;s<n;s++){
+      for(int t=0;t<n;t++){
+        dp[s][t]=min(dp[s][t],dp[s][k]+dp[k][t]);
+        if(dp[s][t]==INF)continue;
+        ans+=dp[s][t];
+      }
+    }
+  }
+
+  cout << ans << endl;
 
 }
