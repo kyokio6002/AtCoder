1f1114c233a4ca0fe719831be1257e06edb65a18
kyokio6002 <kyokio0725@gmail.com>
2021-07-18 00:13:30 +0900

    add progC(virtual)

--- abc204/progC.cpp
+++ abc204/progC.cpp
@@ -1,18 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> edge;
+vector<vector<int>> dist;
+int ans=0;
+
+void dfs(int start,int v){
+  dist[start][v]++;
+  for(auto nv:edge[v]){
+    if(dist[start][nv])continue;
+    // cout << "start:" << start+1 << endl;
+    // cout << "nv:" << nv+1 << endl << endl;
+    ans++;
+    dfs(start,nv);
+  }
+  return;
+}
+
+
 int main(){
 
   int n,m;
   cin >> n >> m;
 
-  vector<vector<int>> edge(n);
+  edge.resize(n);
+  dist.resize(n,vector<int>(n));
+
   int a,b;
   for(int i=0;i<m;i++){
-    cin >> a >> b;a--;b--;
+    cin >> a >> b;
+    a--,b--;
     edge[a].push_back(b);
-    edge[b].push_back(a);
   }
 
+  ans+=n;
+  for(int i=0;i<n;i++){
+    dfs(i,i);
+  }
 
+  cout << ans << endl;
 }
