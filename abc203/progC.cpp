60cc610eefb025d45110007addfd078fb1d8e968
kyokio6002 <kyokio0725@gmail.com>
2021-07-17 22:44:47 +0900

    add progC(virtual)

--- abc203/progC.cpp
+++ abc203/progC.cpp
@@ -7,27 +7,36 @@ int main(){
 
   ll n,k;
   cin >> n >> k;
-  map<ll,ll> mp; 
 
-  int a,b;
+  map<ll,ll> mp;
+  ll a,b;
   for(int i=0;i<n;i++){
     cin >> a >> b;
     mp[a]+=b;
   }
 
+  ll posi,tip;
   ll prio=0;
-  ll pay;
+  ll dist;
   for(auto v:mp){
-    pay=v.first-prio;
-    if(k<pay){
+    posi=v.first;
+    tip=v.second;
+    // ここまでの距離
+    dist=posi-prio;
+    // 到達できる
+    if(k-dist>=0){
+      k-=dist;
+      k+=tip;
+      prio=posi;
+    }else{
       cout << prio+k << endl;
       return 0;
     }
-    k-=pay;
-    prio=v.first;
-    k+=v.second;
+
+    // cout << "posi:" << posi << endl;
+    // cout << "k:" << k << endl;
+    // cout << "prio:" << prio << endl;
   }
 
   cout << prio+k << endl;
-  
 }
