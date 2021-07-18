6789c771962b438c4430c91f2693ac4aa5544e7a
kyokio6002 <kyokio0725@gmail.com>
2021-07-19 00:23:10 +0900

    add progC(virtual)

--- abc189/progC.cpp
+++ abc189/progC.cpp
@@ -2,23 +2,18 @@
 using namespace std;
 
 int main(){
-
+  
   int n;
   cin >> n;
+  vector<int> a(n);
+  for(int i=0;i<n;i++)cin >> a[i];
+
   int ans=0;
-  vector<int> nmax(100005);
-  vector<int> dp(100005);
-  int a;
-  for(int i=0;i<n;i++){
-    cin >> a;
-    for(int j=1;j<=100000;j++){
-      if(j<=a){
-        dp[j]++;
-        nmax[j]=max(dp[j],nmax[j]);
-        ans=max(ans,j*nmax[j]);
-      }else{
-        dp[j]=0;
-      }
+  for(int l=0;l<n;l++){
+    int mina=1001001007;
+    for(int r=l;r<n;r++){
+      mina=min(mina,a[r]);
+      ans=max(ans,mina*(r-l+1));
     }
   }
 
