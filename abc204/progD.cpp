c15587882f522319c7102ec929069e0f20f3380d
kyokio6002 <kyokio0725@gmail.com>
2021-08-17 00:23:25 +0900

    add progD(virtual) AC

--- abc204/progD.cpp
+++ abc204/progD.cpp
@@ -5,18 +5,38 @@ int main(){
 
   int n;
   cin >> n;
+  vector<int> T(n);
+  int sum=0;
+  for(int i=0;i<n;i++)cin >> T[i];
+  for(int i=0;i<n;i++)sum+=T[i];
+  
+  vector<vector<int>> dp(n,vector<int>(sum));
 
-  vector<int> t(n);
-  for(int i=0;i<n;i++)cin >> t[i];
-
-  int oven1=0;
-  int oven2=0;
-  sort(t.rbegin(),t.rend());
-  for(int i=0;i<n;i++){
-    if(oven1<oven2)oven1+=t[i];
-    else oven2+=t[i];
+  dp[0][0]++;
+  for(int i=0;i<n-1;i++){
+    dp[i][T[i]]++;
+    for(int t=0;t<sum;t++){
+      if(dp[i][t]){
+        dp[i+1][t+T[i+1]]++;
+        dp[i+1][t]++;
+      }
+    }
   }
 
-  int ans=max(oven1,oven2);
-  cout << ans << endl;
+  int ans=sum;
+  int ansf;
+  // cout << "sum:" << sum << endl;
+  for(int t=0;t<sum;t++){
+    if(dp[n-1][t]){
+      if(ans>=abs(2*t-sum))ansf=max(t,sum-t);
+      ans=abs(2*t-sum);
+      // cout << "t:" << t << endl;
+      // cout << "abs:" << abs(2*t-sum) << endl;
+      // cout << "ans:" << ans << endl;
+      // cout << "ansf:" << ansf << endl << endl;
+    }
+  }
+
+  cout << ansf << endl;
+
 }
