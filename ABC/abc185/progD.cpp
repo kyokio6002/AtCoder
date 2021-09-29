79dfcbfa16e461a28089801737a3d811d3bcc70b
kyokio6002 <kyokio0725@gmail.com>
2021-09-29 17:43:35 +0900

    modified progD(AC)

--- ABC/abc185/progD.cpp
+++ ABC/abc185/progD.cpp
@@ -1,28 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define INF 1000000007
+#define INF 1001001001
+
 int main(){
 
 	int n,m;
 	cin >> n >> m;
-	vector<int> a(m);
-	int b;
-	for(int i=0;i<m;i++){
-		cin >> b;
-		b--;
-		a[i]=b;
-	}
+  vector<int> a(m);
+  for(int i=0;i<m;i++)cin >> a[i];
+  a.push_back(0);
+  a.push_back(n+1);
 
-	sort(a.begin(),a.end());
-	int bet=INF;
-	int prio=a[0];
-	for(int i=1;i<m;i++){
-		if(a[i]-prio-1!=0)bet=min(bet,a[i]-prio-1);
-		prio=a[i];
-	}
+  sort(a.begin(),a.end());
+  // for(auto v:a)cout << v << ",";
+  // cout << endl;
 
-	cout << "bet:" << bet << endl;
-	int ans=(n-m)/bet;
-	cout << ans << endl;
+  int k=INF;
+  int wid;
+  for(int i=0;i<a.size()-1;i++){
+    wid=a[i+1]-a[i]-1;
+    // cout << "wid:" << wid << endl;
+    if(wid<=0)continue;
+    k=min(k,wid);
+  }
+
+  int ans=0;
+  for(int i=0;i<a.size()-1;i++){
+    wid=a[i+1]-a[i]-1;
+    // cout << "wid:" << wid << endl;
+    if(wid<=0)continue;
+    ans+=wid/k;
+    if(wid%k!=0)ans++;
+  }
+
+  cout << ans << endl;
+  
 }
