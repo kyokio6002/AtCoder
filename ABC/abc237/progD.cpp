67c6a5963a000921824004b19466efa5e84b5895
kyokio6002 <kyokio0725@gmail.com>
2022-02-01 03:59:48 +0900

    add progD.cpp(AC)

--- ABC/abc237/progD.cpp
+++ ABC/abc237/progD.cpp
@@ -5,29 +5,23 @@ int main(){
 
   int n;
   cin >> n;
-  vector<int> a;
-  a.push_back(0);
-  int itr=0;
 
+  list<int> ans(1,0);
+  auto itr=ans.begin();
   for(int i=0;i<n;i++){
     char c;
     cin >> c;
     if(c=='L'){
-      if(itr==0){
-        reverse(a.begin(),a.end());
-        a.push_back(i+1);
-        reverse(a.begin(),a.end());
-        continue;
-      }
-      a.insert(a.begin()+itr,i+1);
+      itr=ans.insert(itr,i+1);
     }else{
-      a.insert(a.begin()+itr+1,i+1);
-      itr+=1;
+      itr++;
+      itr=ans.insert(itr,i+1);
     }
+    // for(auto c:ans)cout << c << " ";
+    // cout << endl;
   }
 
-  for(int i=0;i<=n;i++){
-    cout << a[i] << ' ';
-  }cout << endl;
+  for(auto c:ans)cout << c << " ";
+  cout << endl;
 
 }
