cfc68848cc319786123ee2129c295cc73afab856
kyokio6002 <kyokio0725@gmail.com>
2022-06-25 23:46:03 +0900

    abc257 progD modified(TLE)

--- ABC/abc257/progD.cpp
+++ ABC/abc257/progD.cpp
@@ -9,26 +9,23 @@ struct jump{
 using ll=long long;
 int n;
 vector<jump> jump(200);  // ((x,y),p)
-ll ans=1000000000000000000;
+vector<ll> ans(200);
 
 void dfs(ll s,int now, int count, vector<int> v){
 
-  // cout << "now:" << now << endl;
-  // cout << "count:" << count << endl;
-  // cout << "s:" << s << endl;
-
-  if(count==n){
-    ans=min(ans,s);
-    cout << "route finish" << endl; 
-    for(int i=0;i<n;i++){
-      cout << v[i] << "→";
-    }cout << endl;
-    cout << "s:" << s << endl;
-    return;
-  }
+  // if(count==n){
+  //   cout << "route finish" << endl; 
+  //   for(int i=0;i<n;i++){
+  //     cout << v[i] << "→";
+  //   }cout << endl;
+  //   for(int i=0;i<n;i++){
+  //     printf("ans[%d]:%d\n",i+1,ans[i]);
+  //   }
+  //   return;
+  // }
 
   for(int next=0;next<n;next++){
-    ll len=abs(jump[now].x=jump[next].x)+abs(jump[now].y-jump[next].y);
+    ll len=abs(jump[now].x-jump[next].x)+abs(jump[now].y-jump[next].y);
     // 行ったことあるところはスルー
     if(v[next]!=-1)continue;
     if(next==now)continue;
@@ -36,12 +33,15 @@ void dfs(ll s,int now, int count, vector<int> v){
     if(jump[now].p*s>=len){
       vector<int> nv=v;
       nv[next]=count+1;
+      if(ans[next]>s)ans[next]=s;
       dfs(s,next,count+1,nv);
     }else{
       ll need_s=len/jump[now].p;
       if(need_s*jump[now].p<len)need_s++;
       vector<int> nv=v;
       nv[next]=count+1;
+      if(ans[next]>need_s)ans[next]=need_s;
+      // cout << "next_need_s:" << need_s << endl;
       dfs(need_s,next,count+1,nv);
     }
   }
@@ -55,12 +55,22 @@ int main(){
     cin >> jump[i].x >> jump[i].y >> jump[i].p;
   }
 
+  ll aans=1000000000000000000;
   for(int i=0;i<n;i++){
     vector<int> v(n,-1);
+    vector<ll> reset_ans(n,aans);
+    ans=reset_ans;
+    ans[i]=0;
     v[i]=1;
-    dfs(0,i,1,v);
+    dfs(1,i,1,v);
+    ll now_max=0;
+    for(int j=0;j<n;j++){
+      // printf("ans[%d]:%d\n",i+1,ans[j]);
+      now_max=max(now_max,ans[j]);
+    }
+    aans=min(aans,now_max);
   }
 
-  cout << ans << endl;
+  cout << aans << endl;
 
 }
