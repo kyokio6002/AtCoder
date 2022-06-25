d5a578b74564aab84f290c73b990124b1d6765cf
kyokio6002 <kyokio0725@gmail.com>
2022-06-26 00:21:50 +0900

    abc257 progC modified(AC)

--- ABC/abc257/progC.cpp
+++ ABC/abc257/progC.cpp
@@ -7,9 +7,36 @@ int main(){
    string s;
    cin >> n;
    cin >> s;
-   vector<int> w(n);
-   for(int i=0;i<n;i++)cin >> w[i];
+   map<int,vector<int>> w;
+   int t;
+   int min_w=1000000001;
+   for(int i=0;i<n;i++){
+     cin >> t;
+     w[t].push_back(i);
+     min_w=min(min_w,t);
+   }
 
+   int ans=0;
+   for(int i=0;i<n;i++){
+     if(s[i]=='1')ans++;
+   }
+   int inst_ans=0;
+   inst_ans=ans;
 
+   for(auto v:w){
+     int add=0;
+     // int weight=v.first;
+     vector<int> a=v.second;
+
+     // 更新
+     for(int i=0;i<a.size();i++){
+       if(s[a[i]]=='0')add++;
+       else if(s[a[i]]=='1')add--;
+     }
+     inst_ans+=add;
+     ans=max(ans,inst_ans);
+   }
+
+   cout << ans << endl;
 
 }
