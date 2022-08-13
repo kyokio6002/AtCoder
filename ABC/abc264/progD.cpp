8e7043cef4902134a3012b161416b5e4e26a2119
kyokio6002 <kyokio0725@gmail.com>
2022-08-13 23:17:03 +0900

    update progD(ABC264)

--- ABC/abc264/progD.cpp
+++ ABC/abc264/progD.cpp
@@ -6,4 +6,22 @@ int main(){
   string s;
   cin >> s;
 
+  int ans=0;
+  char tmp;
+  map<char,int> mp;
+  string atcoder="atcoder";
+  for(int i=0;i<atcoder.length();i++)mp[atcoder[i]]=i;
+
+  for(int i=0;i<atcoder.length();i++){
+    int index=s.find(atcoder[i]);
+    while(s[mp[atcoder[i]]]!=atcoder[i]){
+      tmp=s[index-1];
+      s[index-1]=s[index];
+      s[index]=tmp;
+      ans++;
+      index--;
+    }
+  }
+
+  cout << ans << endl;
 }
