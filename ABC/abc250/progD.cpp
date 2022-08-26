0e5fa2783472011025df61b95fae0136d8e3afda
kyokio6002 <kyokio0725@gmail.com>
2022-08-27 02:11:32 +0900

    Fix:progD(ABC250)

--- ABC/abc250/progD.cpp
+++ ABC/abc250/progD.cpp
@@ -22,30 +22,49 @@ int main(){
 
   ll n;
   cin >> n;
-  if(n==1){
-    cout << 0 << endl;
-    return 0;
-  }
 
-  int ans=0;
-  ll nmax = round(cbrt(n));
+  ll ans=0;
+  ll nmax = 1;
+  while(nmax*nmax*nmax*2<=n)nmax++;
   vector<ll> primes;
-  for(int i=0;i<nmax;i++){
-    if(IsPrime(i))primes.push_back(i);
-  }
-  // for(int i=0;i<primes.size();i++){
-  //   cout << primes[i] << ",";
-  // }cout << endl;
+  for(int i=0;i<nmax;i++)if(IsPrime(i))primes.push_back(i);
+  vector<ll> sum_primes(nmax+1);
+  ll prime_count=primes.size();
 
-  for(int pi=0;pi<primes.size()-1;pi++){
-    for(int qi=pi+1;qi<primes.size();qi++){
-      ll k=primes[pi]*primes[qi]*primes[qi]*primes[qi];
-      // cout << "k:" << k << endl;
-      // cout << "q:" << primes[qi] << endl;
-      // cout << "p:" << primes[pi] << endl;
-      if(k<=n)ans++;
-    }
+  for(int i=0;i<prime_count;i++)sum_primes[primes[i]]++;
+
+  ll sum=0;
+  for(int i=0;i<=nmax;i++){
+    sum+=sum_primes[i];
+    sum_primes[i]=sum;
   }
+
+  // cout << endl << "nmax:" << nmax << endl;
+  // // ---- print primes ----
+  // cout << endl << "primes" << endl;
+  // for(int i=0;i<prime_count;i++)cout << primes[i] << ",";cout << endl;
+  // // ---- print sum primes ----
+  // cout << endl << "sum primes" << endl;
+  // for(int i=1;i<nmax+1;i++)cout << i << ",";cout << endl;
+  // for(int i=1;i<nmax+1;i++)cout << "--";cout << endl;
+  // for(int i=1;i<nmax+1;i++)cout << sum_primes[i] << ",";cout << endl;
+
+  for(int qi=1;qi<prime_count;qi++){
+    ll q=primes[qi];
+    ll p=min(q,n/(q*q*q));
+    if(p==q)p=primes[qi-1];
+
+    ll add=sum_primes[p];
+    ans+=add;
+
+    // cout << "n/(q*q*q):" << n/(q*q*q) <<  endl;
+    // cout << "q:" << q << endl;
+    // cout << "p:" << p << endl;
+    // cout << "add:" << add << endl;
+    // cout << "ans:" << ans << endl;
+    // cout << endl;
+  }
+
 
   cout << ans << endl;
 }
