52dfee97513c17f5482c41a2fa5f59d69733fad4
kyokio6002 <kyokio0725@gmail.com>
2022-10-08 01:40:01 +0900

    Fix: ABC270 progD

--- ABC/abc269/progD.cpp
+++ ABC/abc269/progD.cpp
@@ -1,19 +1,53 @@
 #include<bits/stdc++.h>
 #include <utility>
+#include <vector>
 using namespace std;
 
-map<pair<int,int>,int> edge;
-map<pair<int,int>,int> dist;
-int n;
-int ans=0;
+struct UnionFind{
 
-void dfs(int x,int y, int count, int color){
+  vector<int> par;  // par[i]:iの親の番号 ex)par[3]=2→'3の親が2'
+  vector<int> siz;  // 各木のサイズ
 
-  dist[make_pair(x,y)]=color;
+  // コンストラクタ
+  UnionFind(int n): par(n), siz(n,1){
+    for(int i=0;i<n;i++)par[i]=i;
+  }
 
-  cout << "now:" << x << "," << y << endl;
+  // root検索(値を検索する)
+  int root(int x){
+    if(par[x]==x)return x;
+    return par[x]=root(par[x]);
+  }
 
-  vector<pair<int,int>> dire={
+  // 結合
+  void unite(int x,int y){
+    int rx=root(x);
+    int ry=root(y);
+    if(rx==ry)return;
+    par[rx]=ry;
+    siz[ry]+=siz[rx];
+  }
+
+  // 同じ木か確認する
+  bool same(int x,int y){
+    int rx=root(x);
+    int ry=root(y);
+    return rx==ry;
+  }
+
+  // 木のサイズを返す
+  int size(int x){
+    return siz[root(x)];
+  }
+};
+
+bool is_next(int v1,int v2){
+  int v1x=v1/10000;
+  int v1y=v1%10000;
+  int v2x=v2/10000;
+  int v2y=v2%10000;
+
+  vector<pair<int,int>> dist={
     {-1,-1},
     {-1,0},
     {0,-1},
@@ -22,38 +56,46 @@ void dfs(int x,int y, int count, int color){
     {1,1},
   };
 
-  for(int i=0;i<6;i++){
-    int nextx=x+dire[i].first;
-    int nexty=y+dire[i].second;
-    if(dist.count(make_pair(nextx,nexty)))continue;
-    if(!edge.count(make_pair(nextx,nexty)))continue;
-    
-    dfs(nextx,nexty,count+1,color);
+  for(auto d:dist){
+    int addx=d.first;
+    int addy=d.second;
+    if(v1x+addx==v2x && v1y+addy==v2y)return true;
   }
 
+  return false;
 }
-
 
 int main(){
 
+  int n;
   cin >> n;
+  map<pair<int,int>,int> mp;
   int x,y;
+  UnionFind  uni(30002000);
+  vector<int> stack;
   for(int i=0;i<n;i++){
     cin >> x >> y;
-    pair<int,int> p={x,y};
-    edge[p]++;
-    edge[p]++;
+    x+=1000;
+    y+=1000;
+    int mixed=x*10000+y;
+    for(auto v:stack){
+      if(is_next(mixed, v))uni.unite(mixed, v);
+    }
+
+    stack.push_back(mixed);
   }
 
-  int ans=0;
-  for(auto v:edge){
-    int x=v.first.first;
-    int y=v.first.second;
-    // if(dist.count(make_pair(x,y)))continue;
-    dfs(x,y,1,ans);
-    ans++;
+  map<int,int> ans;
+  for(auto v:stack){
+    ans[uni.root(v)]++;
+
+    // int rv=uni.root(v);
+    // int rvx=rv/10000-1000;
+    // int rvy=rv%10000-1000;
+    // int x=v/10000-1000;
+    // int y=v%10000-1000;
+    // printf("(%d,%d): %d,%d\n",x,y,rvx,rvy);
   }
 
-  cout << ans << endl;
-
+  cout << ans.size() << endl;
 }
