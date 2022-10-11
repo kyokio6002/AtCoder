4df46440cb93e3d4253dbbd2166520be0c5d7903
kyokio6002 <kyokio0725@gmail.com>
2022-10-11 23:15:12 +0900

    Fix: progC(ABC271)

--- ABC/abc271/progC.cpp
+++ ABC/abc271/progC.cpp
@@ -6,43 +6,39 @@ int main(){
   int n;
   cin >> n;
   map<long long,long long> mp;
+  const long long ex=1001001001001;
   long long a;
   for(int i=0;i<n;i++){
     cin >> a;
-    mp[a]++;
+    if(mp.count(a))mp[ex]=mp.count(ex)?mp[ex]+1:1;
+    else mp[a]++;
   }
 
-  long long ans=0;
   long long count=1;
-  long long ex=10000000000000;
   while(!mp.empty()){
-    if(!mp.empty() && mp.count(count)){
-      mp[count]--;
-      if(mp[count]!=0)mp[count+ex]=mp[count];
+
+    // cout << count << ":";
+    // for(auto [key,value]:mp)printf("%lld(%d), ",key,value);
+    // cout << endl;
+
+    if(mp.count(count)){
       mp.erase(count);
       count++;
-      ans++;
     }else{
       if(mp.size()<2 && (mp.begin()->second)<2)break;
 
       for(int i=0;i<2;i++){
         long long num=mp.rbegin()->first;
         mp[num]--;
-        if(mp[num]==0)mp.erase(num);
+        if(mp[num]<=0)mp.erase(num);
       }
       count++;
-      ans++;
     }
 
-    // for(auto itr=mp.begin();itr!=mp.end();itr++){
-    //   long long key=itr->first;
-    //   int value=itr->second;
-    //   printf("%lld(%d), ",key,value);
-    // }// cout << endl;
     // for(auto [key,value]:mp)printf("%lld(%d), ",key,value);
     // cout << endl;
   }
 
-  cout << ans << endl;
+  cout << count-1 << endl;
 
 }
