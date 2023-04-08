7aa7ae5f548d9dc807c38bba81425cdcbbff7c63
kyokio6002 <kyokio0725@gmail.com>
2023-04-09 03:05:27 +0900

    Fix: ABC285 progD

--- ABC/abc285/progD.cpp
+++ ABC/abc285/progD.cpp
@@ -6,15 +6,9 @@ int main(){
    int n;
    cin >> n;
    map<string,string> s_t;
-   map<string,int> error;  // 変更先の重複
    for(int i=0;i<n;i++){
      string s1,s2;
      cin >> s1 >> s2;
-     if(error.count(s2)){
-       cout << "No" << endl;
-       return 0;
-     }
-     error[s2]++;
      s_t[s1]=s2;
    }
 
@@ -23,13 +17,17 @@ int main(){
    for(auto [from, to]: s_t){
      // cout << "from:" << from << endl;
      // cout << "to:" << to << endl;
-
+     
+     // 来たことある場所はcontinue
      if(dist.count(from))continue;
      
-     dist[to]++;
+     dist[from]++;
 
+     // toがどこかのfromになっていなければcontinue
      if(!s_t.count(to))continue;
 
+
+     // toがどこかのfromになっていなければたどる
      string next_from=from;
      string next_to=to;
      while(true){
@@ -37,12 +35,13 @@ int main(){
        next_to=s_t[next_from];
        // cout << "next_from:" << next_from << endl;
        // cout << "next_to:" << next_to << endl;
-
-       if(dist.count(next_to)){
+       
+       // next_toがfromに返ってきたらループになる
+       if(next_to==from){
          cout << "No" << endl;
          return 0;
        }
-       dist[next_to]++;
+       dist[next_from]++;
        if(!s_t.count(next_to))break;
      }
    }
