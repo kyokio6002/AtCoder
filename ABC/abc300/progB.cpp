73d026d4b97f60f8e41a31eec3225189175e9121
kyokio6002 <kyokio0725@gmail.com>
2023-06-12 00:31:20 +0900

    Fix: progB(ABC300)

--- ABC/abc300/progB.cpp
+++ ABC/abc300/progB.cpp
@@ -1,96 +1,83 @@
 #include <algorithm>
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
 using namespace std;
 
+int h,w;
+
+void tate_shift(vector<vector<char>> &grid){
+  vector<char> tmp(w);
+
+  for(int y=0;y<h-1;y++){
+    // tmp<-y
+    // y<-y+1
+    // y+1<-tmp
+    for(int x=0;x<w;x++)tmp[x]=grid[x][y];
+    for(int x=0;x<w;x++)grid[x][y]=grid[x][(y+1)%h];
+    for(int x=0;x<w;x++)grid[x][(y+1)%h]=tmp[x];
+  }
+}
+
+void yoko_shift(vector<vector<char>> &grid){
+  vector<char> tmp(h);
+  for(int x=0;x<w-1;x++){
+    // tmp<-x
+    // x<-x+1
+    // x+1<-tmp
+    for(int y=0;y<h;y++)tmp[y]=grid[x][y];
+    for(int y=0;y<h;y++)grid[x][y]=grid[(x+1)%w][y];
+    for(int y=0;y<h;y++)grid[(x+1)%w][y]=tmp[y];
+  }
+}
+
+bool is_sama(vector<vector<char>> &gridA, vector<vector<char>> &gridB){
+  for(int y=0;y<h;y++){
+    for(int x=0;x<w;x++){
+      if(gridA[x][y]!=gridB[x][y])return false;
+    }
+  }
+  return true;
+}
+
 int main(){
 
-  int h,w;
   cin >> h >> w;
 
-  vector<vector<char>> grid1(w,vector<char>(h));
-  vector<vector<char>> grid2(w,vector<char>(h));
-  vector<int> grid1_tate(w);
-  vector<int> grid1_yoko(h);
-  vector<int> grid2_tate(w);
-  vector<int> grid2_yoko(h);
+  vector<vector<char>> gridA(w,vector<char>(h));
+  vector<vector<char>> gridB(w,vector<char>(h));
 
   int count1=0;
   for(int y=0;y<h;y++){
     for(int x=0;x<w;x++){
-      cin >> grid1[x][y];
-      if(grid1[x][y]=='#')count1++;
+      cin >> gridA[x][y];
     }
   }
   int count2=0;
   for(int y=0;y<h;y++){
     for(int x=0;x<w;x++){
-      cin >> grid2[x][y];
-      if(grid2[x][y]=='#')count2++;
+      cin >> gridB[x][y];
     }
   }
 
-  if(count1!=count2){
-    cout << "No" << endl;
-    return 0;
-  }
+  for(int shift_y=0;shift_y<=h;shift_y++){
+    tate_shift(gridA);
+    for(int shift_x=0;shift_x<=w;shift_x++){
+      yoko_shift(gridA);
 
-  for(int y=0;y<h;y++){
-    int yoko_sum1=0;
-    int yoko_sum2=0;
-    for(int x=0;x<w;x++){
-      if(grid1[x][y]=='#')yoko_sum1++;
-      if(grid2[x][y]=='#')yoko_sum2++;
-    }
-    grid1_yoko[y]=yoko_sum1;
-    grid2_yoko[y]=yoko_sum2;
-  }
-  for(int x=0;x<w;x++){
-    int tate_sum1=0;
-    int tate_sum2=0;
-    for(int y=0;y<h;y++){
-      if(grid1[x][y]=='#')tate_sum1++;
-      if(grid2[x][y]=='#')tate_sum2++;
-    }
-    grid1_tate[x]=tate_sum1;
-    grid2_tate[x]=tate_sum2;
-  }
+      // cout << "shift_y:" << shift_y << endl;
+      // cout << "shift_x:" << shift_x << endl;
+      // for(int y=0;y<h;y++){
+      //   for(int x=0;x<w;x++){
+      //     cout << gridA[x][y];
+      //   }cout << endl;
+      // }cout << endl << endl;
 
-  // for(int y=0;y<h;y++)cout << grid1_yoko[y] << ",\n"[y==h-1];
-  // for(int y=0;y<h;y++)cout << grid2_yoko[y] << ",\n"[y==h-1];
-  // for(int x=0;x<w;x++)cout << grid1_tate[x] << ",\n"[x==w-1];
-  // for(int x=0;x<w;x++)cout << grid2_tate[x] << ",\n"[x==w-1];
-
-  sort(grid1_tate.begin(), grid1_tate.end());
-  sort(grid2_tate.begin(), grid2_tate.end());
-  sort(grid1_yoko.begin(), grid1_yoko.end());
-  sort(grid2_yoko.begin(), grid2_yoko.end());
-
-
-  for(int y=0;y<h;y++){
-    if(grid1_yoko[y]!=grid2_yoko[y]){
-      cout << "No" << endl;
-      return 0;
+      if(is_sama(gridA, gridB)){
+        cout << "Yes" << endl;
+        return 0;
+      }
     }
   }
-  for(int x=0;x<w;x++){
-    if(grid1_tate[x]!=grid2_tate[x]){
-      cout << "No" << endl;
-      return 0;
-    }
-  }
-  for(int y=0;y<h;y++){
-    int yoko_sum1=0;
-    int yoko_sum2=0;
-    for(int x=0;x<w;x++){
-      if(grid1[x][y]=='#')yoko_sum1++;
-      if(grid2[x][y]=='#')yoko_sum2++;
-    }
-    grid1_yoko[y]=yoko_sum1;
-    grid2_yoko[y]=yoko_sum2;
-  }
 
-  
-  cout << "Yes" << endl;
-
+  cout << "No" << endl;
 }
